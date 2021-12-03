581ae390ff9deb85a6763a822160db462696e154
Samuel Sweet <samueljsweet@gmail.com>
2021-12-02 21:40:17 -0700

    Support for test input files

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use crate::puzzles::puzzle::Puzzle;
 use crate::puzzles::p1::P1;
 
 fn main() {
-    Puzzle::solve(&P1 {})
+    Puzzle::solve(&P1 {}, false)
 }
--- src/puzzles/puzzle.rs
+++ src/puzzles/puzzle.rs
@@ -5,15 +5,15 @@ pub trait Puzzle<T> {
     fn solve_part_one(&self, data: &Vec<T>);
     fn solve_part_two(&self, data: &Vec<T>);
 
-    fn read_input_file(&self) -> Vec<String> {
+    fn read_input_file(&self, test_input: bool) -> Vec<String> {
         let number = self.number();
-        input_file::read(number)
+        input_file::read(number, test_input)
     }
 
     fn parse_data(&self, raw_data: &Vec<String>) -> Vec<T>;
 
-    fn solve(&self) {
-        let raw_data = self.read_input_file();
+    fn solve(&self, test_input: bool) {
+        let raw_data = self.read_input_file(test_input);
         let data = self.parse_data(&raw_data);
 
         self.solve_part_one(&data);
