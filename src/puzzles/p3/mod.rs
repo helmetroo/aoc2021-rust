33448e1b0279ed41222b3128b3b34222eda3673a
Samuel Sweet <samueljsweet@gmail.com>
2021-12-03 21:43:23 -0700

    - Solution for P3 part 2 - Refactor methods used to solve P3 part 1

--- src/puzzles/p3/mod.rs
+++ src/puzzles/p3/mod.rs
@@ -1,33 +1,92 @@
 use crate::puzzles::puzzle::Puzzle;
 use crate::utils::input_file;
 
-fn power_consumption(report: &Vec<Vec<char>>) -> u32 {
-    let mut gamma_rate = 0u32;
-    let mut epsilon_rate = 0u32;
+pub struct Rates {
+    gamma: u32,
+    epsilon: u32,
+}
 
+impl Rates {
+    pub fn new() -> Self {
+        Self { gamma: 0u32, epsilon: 0u32 }
+    }
+
+    pub fn multiply_values(&self) -> u32 {
+        self.gamma * self.epsilon
+    }
+}
+
+fn power_consumption(report: &Vec<Vec<char>>) -> u32 {
     let bit_width = report[0].len();
     let report_size = report.len();
 
+    (0..bit_width).fold(Rates::new(), |rates, bit| {
+        let (ones, zeroes) = count_bits_at(bit, report_size, report);
+        let bit_to_set = bit_width - bit - 1;
+        let value_to_merge = 1 << bit_to_set;
+        if ones >= zeroes {
+            Rates { gamma: rates.gamma | value_to_merge, ..rates }
+        } else {
+            Rates { epsilon: rates.epsilon | value_to_merge, ..rates }
+        }
+    }).multiply_values()
+}
+
+fn life_support_rating(report: &Vec<Vec<char>>) -> u32 {
+    let bit_width = report[0].len();
+
+    let oxygen_rating = get_rating(report, true, bit_width);
+    let co2_rating = get_rating(report, false, bit_width);
+
+    oxygen_rating * co2_rating
+}
+
+fn get_rating(report: &Vec<Vec<char>>, for_oxygen: bool, bit_width: usize) -> u32 {
+    let mut kept_nums = report.clone();
     for bit in 0..bit_width {
-        let ones_counted = report.iter().fold(0, |acc, number| {
-            let bit_value = number[bit];
-            if bit_value == '1' {
-                acc + 1
+        let (ones, zeroes) = count_bits_at(bit, kept_nums.len(), &kept_nums);
+        kept_nums.retain(|n: &Vec<char>| {
+            let keep_one = if for_oxygen {
+                ones >= zeroes
             } else {
-                acc
-            }
+                ones < zeroes
+            };
+            keep_one && n[bit] == '1' || !keep_one && n[bit] == '0'
         });
 
-        let zeros_counted = report_size - ones_counted;
-        let bit_to_set = bit_width - bit - 1;
-        if ones_counted >= zeros_counted {
-            gamma_rate |= 1 << bit_to_set
-        } else {
-            epsilon_rate |= 1 << bit_to_set
+        if kept_nums.len() == 1 {
+            break;
         }
     }
 
-    gamma_rate * epsilon_rate
+    build_number_from_chars(&kept_nums[0])
+}
+
+fn build_number_from_chars(chars: &Vec<char>) -> u32 {
+    let bit_width = chars.len();
+    (0..bit_width).fold(0u32, |number, bit| {
+        let bit_to_set = bit_width - bit - 1;
+        if chars[bit] == '1' {
+            let value_to_merge = 1 << bit_to_set;
+            number | value_to_merge
+        } else {
+            number
+        }
+    })
+}
+
+fn count_bits_at(bit: usize, report_size: usize, report: &Vec<Vec<char>>) -> (usize, usize) {
+    let ones = report.iter().fold(0, |acc, number| {
+        let bit_value = number[bit];
+        if bit_value == '1' {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+
+    let zeroes = report_size - ones;
+    (ones, zeroes)
 }
 
 pub struct P3;
@@ -44,5 +103,7 @@ impl Puzzle<Vec<char>> for P3 {
         println!("{}", power_consumption(report))
     }
 
-    fn solve_part_two(&self, _report: &Vec<Vec<char>>) {}
+    fn solve_part_two(&self, report: &Vec<Vec<char>>) {
+        println!("{}", life_support_rating(report))
+    }
 }
