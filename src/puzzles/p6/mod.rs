4fbf41613858f4db433623cf5a4f0f3cfea9d739
Samuel Sweet <samueljsweet@gmail.com>
2021-12-10 17:48:01 -0700

    Solution for P6 part 2 (replaces existing exponential running time solution)

--- src/puzzles/p6/mod.rs
+++ src/puzzles/p6/mod.rs
@@ -1,31 +1,32 @@
-use std::iter;
+use std::collections::HashMap;
 
 use crate::puzzles::puzzle::Puzzle;
 
-#[warn(unused_assignments)]
-fn count_fish(initial_timers: &Vec<u32>, iterations: u32) -> usize {
-    let mut final_timers = initial_timers.clone();
-    for _ in 0..iterations {
-        let mut zeroes_counted = 0;
-        for timer in final_timers.iter_mut() {
-            let zero = &mut 0;
-            if timer > zero {
-                *timer -= 1;
-            } else {
-                zeroes_counted += 1;
-                *timer = 6;
-            }
+fn count_fish(initial_timers: &Vec<u32>, days: usize) -> u64 {
+    // Hint from https://www.reddit.com/r/adventofcode/comments/rdfv7n/comment/ho0sqgs/?utm_source=share&utm_medium=web2x&context=3 to hold a dict of timers
+    let initial_histogram: HashMap<_, _> =
+        initial_timers.into_iter().fold(HashMap::new(), |mut h, t| {
+            h.entry(*t).and_modify(|c| *c += 1).or_insert(1u64);
+            h
+        });
+
+    let final_histogram = (1..=days).into_iter().fold(initial_histogram, |mut h, _| {
+        let new_fish = *h.entry(0).or_insert(0u64);
+
+        for n in 1..=8 {
+            let fish_in_n = *h.entry(n).or_insert(0u64);
+            h.entry(n).and_modify(|c| *c -= fish_in_n).or_insert(0u64);
+            h.entry(n - 1).and_modify(|c| *c += fish_in_n).or_insert(0u64);
         }
 
-        final_timers.extend(
-            iter::repeat(8u32)
-                .take(zeroes_counted)
-                .collect::<Vec<u32>>(),
-        );
-        zeroes_counted = 0;
-    }
+        h.entry(0).and_modify(|c| *c -= new_fish).or_insert(0u64);
+        h.entry(6).and_modify(|c| *c += new_fish).or_insert(0u64);
+        h.entry(8).and_modify(|c| *c += new_fish).or_insert(0u64);
 
-    final_timers.len()
+        h
+    });
+
+    final_histogram.values().sum()
 }
 
 pub struct P6;
@@ -44,5 +45,8 @@ impl Puzzle<Vec<u32>> for P6 {
         println!("{}", count);
     }
 
-    fn solve_part_two(&self, _timers: &Vec<u32>) {}
+    fn solve_part_two(&self, initial_timers: &Vec<u32>) {
+        let count = count_fish(initial_timers, 256);
+        println!("{}", count);
+    }
 }
