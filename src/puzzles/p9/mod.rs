ed074af864d86e7b1d843901468aaa9ee3f1e736
Samuel Sweet <samueljsweet@gmail.com>
2021-12-15 18:09:38 -0700

    Solution for P9 part 2

--- src/puzzles/p9/mod.rs
+++ src/puzzles/p9/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 use crate::puzzles::puzzle::Puzzle;
@@ -11,6 +12,7 @@ struct Edge {
     to: Position,
 }
 type AdjacencyList = Vec<Edge>;
+type AdjacencyGraph = HashMap<Position, Vec<Position>>;
 type PosSet = HashSet<Position>;
 
 fn build_adjacency_list(height_map: &HeightMap) -> AdjacencyList {
@@ -55,6 +57,25 @@ fn build_adjacency_list(height_map: &HeightMap) -> AdjacencyList {
     adjacency_list
 }
 
+fn build_inv_adjacency_graph(adjacency_list: &AdjacencyList, height_map: &HeightMap) -> AdjacencyGraph {
+    adjacency_list
+        .into_iter()
+        .fold(AdjacencyGraph::new(), |mut graph, edge| {
+            let Edge { from, to } = edge;
+            let low_enough = height_map[from.0][from.1] < 9;
+            if low_enough {
+                // Must be indirected (hence the redundant to -> from, from -> to)
+                let adjacents = graph.entry(*to).or_insert(Vec::new());
+                adjacents.push(*from);
+
+                let adjacents = graph.entry(*from).or_insert(Vec::new());
+                adjacents.push(*to);
+            }
+
+            graph
+        })
+}
+
 fn build_positions_set(height_map: &HeightMap) -> PosSet {
     let rows = height_map.len();
     let cols = height_map[0].len();
@@ -83,6 +104,48 @@ fn sum_risk_levels(height_map: &HeightMap) -> u32 {
         .sum()
 }
 
+fn find_basins(sinks: &Vec<Position>, adjacency_graph: &AdjacencyGraph) -> Vec<PosSet> {
+    sinks
+        .iter()
+        .map(|sink| compute_basin(sink, adjacency_graph))
+        .collect()
+}
+
+// Depth-first search to get all the nodes from the sink
+fn compute_basin(sink: &Position, adjacency_graph: &AdjacencyGraph) -> PosSet {
+    let mut basin = PosSet::new();
+    let mut to_visit = adjacency_graph[sink].clone();
+    while !to_visit.is_empty() {
+        to_visit.pop().and_then(|next| {
+            let unvisited = !basin.contains(&next);
+            if unvisited {
+                basin.insert(next);
+                if adjacency_graph.contains_key(&next) {
+                    to_visit.extend(adjacency_graph[&next].clone());
+                }
+            }
+
+            Some(())
+        });
+    }
+
+    basin
+}
+
+fn mul_largest_three_basins(height_map: &HeightMap) -> u32 {
+    let adjacency_list = build_adjacency_list(height_map);
+    let adjacency_graph = build_inv_adjacency_graph(&adjacency_list, &height_map);
+    let sinks = find_sink_positions(height_map, &adjacency_list);
+    let mut basins = find_basins(&sinks, &adjacency_graph);
+
+    // Descending order by num nodes
+    basins.sort_by(|a, b| b.len().cmp(&a.len()));
+
+    basins[0..3]
+        .iter()
+        .fold(1u32, |acc, basin| acc * (basin.len() as u32))
+}
+
 pub struct P9;
 impl Puzzle<HeightMap> for P9 {
     fn number(&self) -> u8 {
@@ -101,5 +164,8 @@ impl Puzzle<HeightMap> for P9 {
         println!("{}", sum_risk_levels);
     }
 
-    fn solve_part_two(&self, _height_map: &HeightMap) {}
+    fn solve_part_two(&self, height_map: &HeightMap) {
+        let prod_largest_three_basins = mul_largest_three_basins(height_map);
+        println!("{}", prod_largest_three_basins);
+    }
 }
