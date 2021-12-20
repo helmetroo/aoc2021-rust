110243da9fc41e89f84d1df5a5a0849d4660555a
Samuel Sweet <samueljsweet@gmail.com>
2021-12-19 22:08:14 -0700

    Solution for P11 part 2

--- src/puzzles/p11/mod.rs
+++ src/puzzles/p11/mod.rs
@@ -9,58 +9,82 @@ type PathIncr = (isize, isize);
 type CellSet = HashSet<Position>;
 type AdjacentCells = Vec<Position>;
 
-fn count_flashes(energy_map: &EnergyMap, steps: usize) -> u32 {
-    let rows = energy_map.len();
-    let cols = energy_map[0].len();
-    let mut flashes = 0;
-    let mut final_map = energy_map.clone();
+fn count_flashes(map: &EnergyMap, steps: usize) -> u32 {
+    let mut final_map = map.clone();
+    let mut last_flashes = 0;
 
     for _step in 0..steps {
-        let mut flashed_cells = CellSet::new();
+        last_flashes += iterate(&mut final_map);
+    }
 
-        // Increment energies
-        for row in 0..rows {
-            for col in 0..cols {
-                let cell = &mut final_map[row][col];
-                *cell += 1;
+    last_flashes
+}
 
-                if *cell > 9 {
-                    flashed_cells.insert((row, col));
-                }
+fn find_step_with_all_flashing(map: &EnergyMap) -> usize {
+    let mut final_map = map.clone();
+    let mut step = 0;
+    let mut found_all_flashing = false;
+
+    while !found_all_flashing {
+        iterate(&mut final_map);
+        found_all_flashing = are_all_flashing(&final_map);
+        step += 1;
+    }
+
+    step
+}
+
+fn are_all_flashing(map: &EnergyMap) -> bool {
+    map.iter().flat_map(|row| row.iter()).all(|&val| val == 0)
+}
+
+fn iterate(map: &mut EnergyMap) -> u32 {
+    let rows = map.len();
+    let cols = map[0].len();
+    let mut flashed_cells = CellSet::new();
+
+    // Increment energies
+    for row in 0..rows {
+        for col in 0..cols {
+            let cell = &mut map[row][col];
+            *cell += 1;
+
+            if *cell > 9 {
+                flashed_cells.insert((row, col));
+            }
+        }
+    }
+
+    // Increment energies of those adjacent to those that first flashed
+    let mut adjacents = get_adjacents(&flashed_cells, rows, cols);
+    while !adjacents.is_empty() {
+        let mut all_new_adjacents = AdjacentCells::new();
+        for &pos in adjacents.iter() {
+            if flashed_cells.contains(&pos) {
+                continue;
+            }
+
+            let (row, col) = pos;
+            let cell = &mut map[row][col];
+            *cell += 1;
+
+            if *cell > 9 {
+                flashed_cells.insert(pos);
+                let new_adjacents = get_adjacents_from(pos, rows, cols);
+                all_new_adjacents.extend(new_adjacents);
             }
         }
 
-        // Increment energies of those adjacent to those that first flashed
-        let mut adjacents = get_adjacents(&flashed_cells, rows, cols);
-        while !adjacents.is_empty() {
-            let mut all_new_adjacents = AdjacentCells::new();
-            for &pos in adjacents.iter() {
-                if flashed_cells.contains(&pos) {
-                    continue;
-                }
+        adjacents.clear();
+        adjacents.extend(all_new_adjacents);
+    }
 
-                let (row, col) = pos;
-                let cell = &mut final_map[row][col];
-                *cell += 1;
-
-                if *cell > 9 {
-                    flashed_cells.insert(pos);
-                    let new_adjacents = get_adjacents_from(pos, rows, cols);
-                    all_new_adjacents.extend(new_adjacents);
-                }
-
-            }
-
-            adjacents.clear();
-            adjacents.extend(all_new_adjacents);
-        }
-
-        // Reset and count flashed
-        for (row, col) in flashed_cells {
-            let cell = &mut final_map[row][col];
-            flashes += 1;
-            *cell = 0;
-        }
+    // Reset and count flashed
+    let mut flashes = 0;
+    for (row, col) in flashed_cells {
+        let cell = &mut map[row][col];
+        flashes += 1;
+        *cell = 0;
     }
 
     flashes
@@ -121,10 +145,13 @@ impl Puzzle<EnergyMap> for P11 {
             .collect::<EnergyMap>()
     }
 
-    fn solve_part_one(&self, energy_map: &EnergyMap) {
-        let flashes = count_flashes(energy_map, 100);
+    fn solve_part_one(&self, map: &EnergyMap) {
+        let flashes = count_flashes(map, 100);
         println!("{}", flashes);
     }
 
-    fn solve_part_two(&self, _energy_map: &EnergyMap) {}
+    fn solve_part_two(&self, map: &EnergyMap) {
+        let step = find_step_with_all_flashing(map);
+        println!("{}", step);
+    }
 }
